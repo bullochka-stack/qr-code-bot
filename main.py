c3e574d089f873a2ab446fd0c320f481d4fffaf5
Bullochka <tamer.kab@mail.ru>
2022-10-28 21:53:21 +0200

    Deploy

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+
+import qrcode
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+TOKEN = '5604738869:AAFcAr07axPGpbSXK_E6nEara9nrl9pXTzo'#f'{os.getenv("TOKEN")}'
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(msg: types.Message):
+    await msg.answer(f'Я бот-генератор QR-кодов. Приятно познакомиться, {msg.from_user.first_name}. Отправь мне ссылку или текст и я тебе пришлю код.')
+
+
+@dp.message_handler()
+async def send_code(msg: types.Message):
+    data = msg.text
+    img = qrcode.make(data)
+    img.save(f'media/{msg.from_user.id}-{msg.date.date()}.png')
+    img = open(f'media/{msg.from_user.id}-{msg.date.date()}.png', 'rb')
+    await msg.answer_photo(img, caption=f'Ваш QR-код: {msg.text}')
+
+    os.remove(f'media/{msg.from_user.id}-{msg.date.date()}.png')
+
+
+if __name__ == '__main__':
+   executor.start_polling(dp)
